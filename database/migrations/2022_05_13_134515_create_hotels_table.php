8234fb1aa9ef740fdf8b365a7dd3d9bfea82ae77
tarek-elhak <tarekroja123@gmail.com>
2022-05-13 16:49:37 +0200

    create hotels,rooms,hotel_images and room_images tables

---
+++ database/migrations/2022_05_13_134515_create_hotels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("type");
+            $table->string("city");
+            $table->string("distance_from_center");
+            $table->text("description");
+            $table->tinyInteger("rating");
+            $table->boolean("featured")->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hotels');
+    }
+};
